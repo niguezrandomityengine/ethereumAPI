b1f76ce977e50a79215071fda0c8edc3ba58189e
Niguez Randomity Engine <46820985+niguezrandomityengine@users.noreply.github.com>
2019-06-14 16:14:36 +0530

    Update tcnreAPI.sol

--- tcnreAPI.sol
+++ tcnreAPI.sol
@@ -1,1 +1,162 @@
+/*
 
+Ñíguez Randomity Engine API for ThunderCore
+
+MIT License
+
+Copyright (c) 2019 niguezrandomityengine | Programmed and designed by Scheich R. Ahmed
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+pragma solidity >= 0.4.0; // Compiler version incompatible error!
+
+contract niguezRandomityEngine {
+
+  function ra() external view returns (uint256);
+	function rb() external view returns (uint256);
+	function rc() external view returns (uint256);
+	function rd() external view returns (uint256);
+	function re() external view returns (uint256);
+	function rf() external view returns (uint256);
+	function rg() external view returns (uint256);
+	function rh() external view returns (uint256);
+	function ri() external view returns (uint256);
+	function rj() external view returns (uint256);
+	function rk() external view returns (uint256);
+	function rl() external view returns (uint256);
+	function rm() external view returns (uint256);
+	function rn() external view returns (uint256);
+	function ro() external view returns (uint256);
+	function rp() external view returns (uint256);
+	function rq() external view returns (uint256);
+	function rr() external view returns (uint256);
+	function rs() external view returns (uint256);
+	function rt() external view returns (uint256);
+	function ru() external view returns (uint256);
+	function rv() external view returns (uint256);
+	function rw() external view returns (uint256);
+	function rx() external view returns (uint256);
+}
+
+contract usingNRE {
+
+  niguezRandomityEngine internal nre = niguezRandomityEngine(0xA9EfBB7c3FE6410eB3edae1B97205209Cd41bA09);
+    
+  function ra() internal view returns (uint256) {
+        return nre.ra();
+    }
+	
+	function rb() internal view returns (uint256) {
+        return nre.rb();
+    }
+	
+	function rc() internal view returns (uint256) {
+        return nre.rc();
+    }
+	
+	function rd() internal view returns (uint256) {
+        return nre.rd();
+    }
+	
+	function re() internal view returns (uint256) {
+        return nre.re();
+    }
+	
+	function rf() internal view returns (uint256) {
+        return nre.rf();
+    }
+	
+	function rg() internal view returns (uint256) {
+        return nre.rg();
+    }
+	
+	function rh() internal view returns (uint256) {
+        return nre.rh();
+    }
+	
+	function ri() internal view returns (uint256) {
+        return nre.ri();
+    }
+	
+	function rj() internal view returns (uint256) {
+        return nre.rj();
+    }
+	
+	function rk() internal view returns (uint256) {
+        return nre.rk();
+    }
+	
+	function rl() internal view returns (uint256) {
+        return nre.rl();
+    }
+	
+	function rm() internal view returns (uint256) {
+        return nre.rm();
+    }
+	
+	function rn() internal view returns (uint256) {
+        return nre.rn();
+    }
+	
+	function ro() internal view returns (uint256) {
+        return nre.ro();
+    }
+	
+	function rp() internal view returns (uint256) {
+        return nre.rp();
+    }
+	
+	function rq() internal view returns (uint256) {
+        return nre.rq();
+    }
+	
+	function rr() internal view returns (uint256) {
+        return nre.rr();
+    }
+	
+	function rs() internal view returns (uint256) {
+        return nre.rs();
+    }
+	
+	function rt() internal view returns (uint256) {
+        return nre.rt();
+    }
+	
+	function ru() internal view returns (uint256) {
+        return nre.ru();
+    }
+	
+	function rv() internal view returns (uint256) {
+        return nre.rv();
+    }
+	
+	function rw() internal view returns (uint256) {
+        return nre.rw();
+    }
+	
+	function rx() internal view returns (uint256) {
+        return nre.rx();
+    }
+}
+
+/*
+End of API
+*/
